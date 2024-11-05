377b17dac0642f329fd83cfa663b078d6dd35cf4
pranay-vohra <pranayvohra77@gmail.com>
2024-11-05 17:29:55 +0530

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by pranay vohra on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LengthConversionTests/LengthConversionTests.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionTests.swift
+//  LengthConversionTests
+//
+//  Created by pranay vohra on 05/11/24.
+//
+
+import Testing
+@testable import LengthConversion
+
+struct LengthConversionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
